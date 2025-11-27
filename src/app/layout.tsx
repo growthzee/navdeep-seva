075d1e44812560fb042fb7a3fffeafa925ffcc9f
Ayan Pakhira <120088765+ayan471@users.noreply.github.com>
2025-11-27 12:02:25 +0530

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,16 +31,16 @@ export default function RootLayout({
         {children}
         {/* Google Analytics */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-RY4S3NHMZ4"
+          src="https://www.googletagmanager.com/gtag/js?id=G-97W969J1N6"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
+             window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-RY4S3NHMZ4');
+  gtag('config', 'G-97W969J1N6');
           `}
         </Script>
       </body>
